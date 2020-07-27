eebbb31e60885bfe3163139e062735e2650af480
Sameera Leola <sameeraleola@gmail.com>
2020-07-26 23:19:48 -0700

    Completed Fold an array

--- foldarray.playground/Contents.swift
+++ foldarray.playground/Contents.swift
@@ -1,22 +1,46 @@
 import UIKit
 
-func foldArray(_ arr: [Int], times: Int){
-//func foldArray(_ arr: [Int], times: Int) -> [Int] {
+func foldArray(_ arr: [Int], times: Int) -> [Int] {
     var foldedArray: [Int] = []
     var array = arr
-    var index = 0
-    
-    while !array.isEmpty {
-//        foldedArray.append(array.first! + array.last!)
-        print(array[index])
-        array[index]
-        index += 1
-
+    var t = times
+    print(arr.count)
+    if arr.count == 1 {
+        foldedArray.append(contentsOf: arr)
+        return foldedArray
     }
     
-//    return array
-    
+    while t > 0 {
+        while !array.isEmpty {
+            
+            if array.count == 1 {
+                foldedArray.append(contentsOf: array)
+                break
+            }
+            
+            foldedArray.append(array.first! + array.last!)
+            array.removeFirst()
+            array.removeLast()
+        }
+        t -= 1
+        if t > 0 {
+            array = foldedArray
+            foldedArray = []
+        }
+    }
+    return foldedArray
 }
 
-foldArray([1, 2, 3, 4, 5], times: 1)
 //print(foldArray([1, 2, 3, 4, 5], times: 1))
+//print(foldArray([1, 2, 3, 4, 5], times: 2))
+print(foldArray([2], times: 1))
+//print(foldArray([74, 76, 81, 92, 104], times: 1))
+
+
+
+
+
+
+
+
+
