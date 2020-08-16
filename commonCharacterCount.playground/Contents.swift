8ea6db9e0635fde99a6d6e0921d9dba7dd4cd330
Sameera Leola <sameeraleola@gmail.com>
2020-08-15 21:49:28 -0500

    Working on challenge

--- commonCharacterCount.playground/Contents.swift
+++ commonCharacterCount.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var str = "Hello, playground"
+//let alpha = 'abcdefghijklmnopqrstuvwxyz'
+let s1 = "aabcc"
+let s2 = "adcaa"
+
+var commonCount = 0
+
+var common = [Character : Int]()
+
+// Populate the dictionary with the values in the string
+for c in s1 {
+    if let val = common[c] {
+        common[c] = val + 1
+    } else {
+        common[c] = 1
+    }
+    print("common = \(common)")
+}
