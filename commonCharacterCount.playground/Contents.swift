e8ce04383d490f81f393e0f9ebcfda59be12ed09
Sameera Leola <sameeraleola@gmail.com>
2020-08-15 23:47:32 -0500

    Solved

--- commonCharacterCount.playground/Contents.swift
+++ commonCharacterCount.playground/Contents.swift
@@ -1,42 +1,22 @@
 import UIKit
 
+func commonCharacterCount(s1: String, s2: String) -> Int {
+    var s1Chars = Array(s1)
+    var s2Chars = Array(s2)
 
-var s1 = "aabcc"
-var s2 = "adcaa"
-
-var s1Chars = Array(s1)
-
-var commonCount = 0
-
-// Using a dictionary
-//var common = [Character : Int]()
-//
-//// Populate the dictionary with the values in the string
-//for c in s1 {
-//    if let val = common[c] {
-//        common[c] = val + 1
-//    } else {
-//        common[c] = 1
-//    }
-//    print("common = \(common)")
-//}
-
-for c in s1Chars {
-//    if s2.contains(c) {
-//        commonCount += 1
-//
-//        print("c = \(c)")
-//    }
-    if let i = s2.firstIndex(of: c) {
-        commonCount += 1
-        s2.remove(at: i)
-        s1.removeFirst()
-        print("s1 = \(s1) :: s2 = \(s2)")
-    }
-}
-print("The common count = \(commonCount)")
+    var commonCount = 0
     
+    for c in s1Chars {
+        if let i = s2Chars.firstIndex(of: c) {
+            commonCount += 1
+            s2Chars.remove(at: i)
+            s1Chars.removeFirst()
+        }
+    }
+    return commonCount
+}
 
-
+print(commonCharacterCount(s1: "aabcc", s2: "adcaa" )) // 3
+print(commonCharacterCount(s1: "zzzz", s2: "zzzzzzz" )) // 4
 
 
