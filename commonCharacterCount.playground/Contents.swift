80df63ddbdafde453b93957a4361a73d732c58cd
Sameera Leola <sameeraleola@gmail.com>
2020-08-15 23:10:00 -0500

    Exploring more solutions to common character count

--- commonCharacterCount.playground/Contents.swift
+++ commonCharacterCount.playground/Contents.swift
@@ -1,19 +1,42 @@
 import UIKit
 
-//let alpha = 'abcdefghijklmnopqrstuvwxyz'
-let s1 = "aabcc"
-let s2 = "adcaa"
+
+var s1 = "aabcc"
+var s2 = "adcaa"
+
+var s1Chars = Array(s1)
 
 var commonCount = 0
 
-var common = [Character : Int]()
+// Using a dictionary
+//var common = [Character : Int]()
+//
+//// Populate the dictionary with the values in the string
+//for c in s1 {
+//    if let val = common[c] {
+//        common[c] = val + 1
+//    } else {
+//        common[c] = 1
+//    }
+//    print("common = \(common)")
+//}
 
-// Populate the dictionary with the values in the string
-for c in s1 {
-    if let val = common[c] {
-        common[c] = val + 1
-    } else {
-        common[c] = 1
+for c in s1Chars {
+//    if s2.contains(c) {
+//        commonCount += 1
+//
+//        print("c = \(c)")
+//    }
+    if let i = s2.firstIndex(of: c) {
+        commonCount += 1
+        s2.remove(at: i)
+        s1.removeFirst()
+        print("s1 = \(s1) :: s2 = \(s2)")
     }
-    print("common = \(common)")
 }
+print("The common count = \(commonCount)")
+    
+
+
+
+
