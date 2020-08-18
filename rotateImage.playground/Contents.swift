6c80bcdb8b056c058839c49af607011957e9fc4c
Sameera Leola <sameeraleola@gmail.com>
2020-08-18 09:11:05 -0500

    Corrected the row/column replacement

--- rotateImage.playground/Contents.swift
+++ rotateImage.playground/Contents.swift
@@ -5,15 +5,21 @@ import UIKit
 
 //func rotateImage(a: [[Int]]) -> [[Int]] {
 func rotateImage(a: [[Int]]) {
-    var temp = 0
-    for r in a {
-        for c in r {
-            temp = c
-            
-                        print(temp)
+    var img = a
+    var tmp = 0
+    for r in 0 ..< a.count {
+        for c in r ..< a.count {
+            tmp =  img[r][c]
+            print("tmp = [\(r)][\(c)] = \(tmp)")
+            print("img[\(c)][\(r)] -> img[\(r)][\(c)] = \(img[c][r]) replaces \(img[r][c])")
+            img[r][c] = img[c][r]
+            print("tmp -> img[\(c)][\(r)] = \(tmp) replaces \(img[c][r])")
+            img[c][r] = tmp
+            print("Current rotation: \(img)\n---")
         }
     }
-   
+    print("The original image = \(a)")
+    print("The rotated image = \(img)")
 }
 
 rotateImage(a: [[1,2,3], [4,5,6], [7,8,9]])
