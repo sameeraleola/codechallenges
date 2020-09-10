28bf77fb6e51666463a8c426f4c98a3e876aabd1
Sameera Leola <sameeraleola@gmail.com>
2020-09-10 05:45:12 -0500

    Built out linked list using challenge class init

--- removeNthNodeFromEndofList.playground/Contents.swift
+++ removeNthNodeFromEndofList.playground/Contents.swift
@@ -9,24 +9,47 @@ import UIKit
      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
  }
 
-//func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
-func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> Int {
-    
-    var nodeCount = 0
-    
-    // If the list is empty return
-//    guard let currNode = head else { return head }
-    guard let currNode = head else { return 0 }
+// Build out the singly linked list one node at a time!
+var list = ListNode(1)
+var head = list
+head.next = ListNode(2)
+head.next?.self.next = ListNode(3)
+head.next?.next?.self.next = ListNode(4)
+head.next?.next?.next?.self.next = ListNode(5)
+head.next?.next?.next?.next?.self.next = ListNode(6)
+head.next?.next?.next?.next?.next?.self.next = ListNode(7)
 
-    // Count the nodes
-    while let _ = currNode.next {
-        nodeCount += 1
-    }
-    
-    // Go to the node to be removed
-    
-    // Check if we are removing the head
-   return nodeCount
+print(head.val, head.next?.val ?? -1, head.next?.next?.val ?? -1, head.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.val ?? -1,
+      head.next?.next?.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.next?.next?.val ?? -1)
+
+func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+    let head = list
+    return head
 }
 
-print(removeNthFromEnd(_ head: ListNode?, _ n: Int)
+print("\(removeNthFromEnd(list, 0)?.val ?? -1)")
+////func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> Int {
+//
+//    var nodeCount = 0
+//
+//    // If the list is empty return
+//    guard let currNode = head else { print("I'm returning"); return head }
+//    print(currNode.val)
+////    guard let currNode = head else { return 0 }
+//
+//    // Count the nodes
+//    while currNode.next != nil {
+//        print("Start counting nodes")
+//        nodeCount += 1
+//         //= currNode.next
+//        print("Counting nodes")
+//    }
+//
+//    // Go to the node to be removed
+//
+//    // Check if we are removing the head
+//    print("nodeCount = \(nodeCount)")
+//    return head
+//}
+
+
