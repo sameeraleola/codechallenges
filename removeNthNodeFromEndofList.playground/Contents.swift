817d487d5a278dafa4868f3dd6bad3f4938e7ecb
Sameera Leola <sameeraleola@gmail.com>
2020-09-11 00:31:00 -0500

    Playing with linked lists

--- removeNthNodeFromEndofList.playground/Contents.swift
+++ removeNthNodeFromEndofList.playground/Contents.swift
@@ -7,6 +7,10 @@ import UIKit
      public init() { self.val = 0; self.next = nil; }
      public init(_ val: Int) { self.val = val; self.next = nil; }
      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    
+    func printList() {
+        print("Hello world!")
+    }
  }
 
 // Build out the singly linked list one node at a time!
@@ -16,18 +20,41 @@ head.next = ListNode(2)
 head.next?.self.next = ListNode(3)
 head.next?.next?.self.next = ListNode(4)
 head.next?.next?.next?.self.next = ListNode(5)
-head.next?.next?.next?.next?.self.next = ListNode(6)
-head.next?.next?.next?.next?.next?.self.next = ListNode(7)
+//head.next?.next?.next?.next?.self.next = ListNode(6)
+//head.next?.next?.next?.next?.next?.self.next = ListNode(7)
 
-print(head.val, head.next?.val ?? -1, head.next?.next?.val ?? -1, head.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.val ?? -1,
-      head.next?.next?.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.next?.next?.val ?? -1)
+//print(head.val, head.next?.val ?? -1, head.next?.next?.val ?? -1, head.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.val ?? -1,
+//      head.next?.next?.next?.next?.next?.val ?? -1, head.next?.next?.next?.next?.next?.next?.val ?? -1)
 
 func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+//func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> Int? {
     let head = list
+    
+    var nodeCount = 0
+    var currNode : ListNode? = head
+    
+    while (currNode != nil) {
+        nodeCount += 1
+        currNode = currNode?.next
+    }
+    
+    print("Delete node \(n)")
+    print("The value of the node \(n) from the end is \(nodeCount - 2)")
+
+    currNode = head
+    
+    while (currNode < nodeCount) {
+        print("The current node contains: \(currNode?.val ?? -1)")
+        currNode = currNode?.next
+    }
+    
     return head
 }
 
-print("\(removeNthFromEnd(list, 0)?.val ?? -1)")
+//print("\(removeNthFromEnd(list, 0)?.val ?? -1)")
+print("The value in the first node is \(removeNthFromEnd(list, 2)?.val ?? -1)")
+
+
 ////func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> Int {
 //
 //    var nodeCount = 0
