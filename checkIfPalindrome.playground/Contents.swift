af5c059d124218d683befeedf2b544883cb4f12d
Sameera Leola <sameeraleola@gmail.com>
2020-09-15 19:11:19 -0500

    Check if palindrome

---
+++ checkIfPalindrome.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+func isPalindrome(_ myString: String) -> Bool {
+    let reverseString = String(myString.reversed())
+    if(myString != "" && myString == reverseString) {
+        return true
+    } else {
+        return false
+    }
+}
+
+print(isPalindrome("racecar"))
