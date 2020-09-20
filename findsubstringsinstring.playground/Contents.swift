752313bbfc7a5826cd8aa594f4aa6a81c85fd0db
Sameera Leola <sameeraleola@gmail.com>
2020-09-19 21:01:29 -0500

    Starting more code challenges

---
+++ findsubstringsinstring.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+/*
+ This is in Java
+ https://www.javacodeexamples.com/java-count-occurrences-of-substring-in-string-example/724
+ 
+ startIndex = index of the first character
+ endIndex = index after the last character
+ after = index of the character after the given index
+ before = index of the character before the given index
+ 
+ package com.javacodeexamples.stringexamples;
+  
+ public class CountOccurrencesOfSubstringExample {
+     public static void main(String[] args) {
+         String str = "JavaExamplesJavaCodeJavaProgram";
+         String strFind = "Java";
+         int count = 0, fromIndex = 0;
+         
+         while ((fromIndex = str.indexOf(strFind, fromIndex)) != -1 ){
+             System.out.println("Found at index: " + fromIndex);
+             count++;
+             fromIndex++;
+         }
+         System.out.println("Total occurrences: " + count);
+     }
+ }
+ */
+
+//func countOccurancesOfSubstring() {
+//    let str = "JavaExamplesJavaCodeJavaProgram"
+var str = "Hello, playground"
+
+var index = str.firstIndex(of: ",")
+print(str[index])
