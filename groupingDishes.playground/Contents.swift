56152839bbd6266a8332a2bce4d311642a8506dc
Sameera Leola <sameeraleola@gmail.com>
2020-09-28 02:26:50 -0500

    Don't understand groupingDishes yet

--- groupingDishes.playground/Contents.swift
+++ groupingDishes.playground/Contents.swift
@@ -1,31 +1,38 @@
 import UIKit
 
-var ingredients = [String]()
-var dish = [String]()
-
 let dishes = [
-    "Salad" : ["Tomato","Cucumber","Salad","Sauce"],
-    "Pizza" : ["Tomato","Sausage","Sauce","Dough"],
-    "Quesadilla" : ["Chicken","Cheese","Sauce"],
-    "Sandwich" : ["Salad","Bread","Tomato","Cheese"]
+    ["Salad", "Tomato","Cucumber","Salad","Sauce"],
+    ["Pizza", "Tomato","Sausage","Sauce","Dough"],
+    ["Quesadilla", "Chicken","Cheese","Sauce"],
+    ["Sandwich", "Salad","Bread","Tomato","Cheese"]
 ]
 
-var groupingDishes : [String : String] = [ : ]
+func groupingDishes(dishes: [[String]]) -> [[String]] {
+    var map = [String:[String]]()
 
-for (dishName, ingredient) in dishes {
-    for ingredientName in ingredient {
-        print("ingredientName = \(ingredientName)")
+    for i in 0 ..< dishes.count {
+        let dish = dishes[i][0]
+        for j in 1..<dishes[i].count {
+            if map[dishes[i][j]] == nil {
+                map[dishes[i][j]] = [dish]
+             } else {
+                map[dishes[i][j]]?.append(dish)
+            }
+        }
     }
-    
-    
-//    if ingredients.keys.contains(dishName) {
-//    if !dish.contains(dishName) {
-//        dish.append(dishName)
-//    }
-//    print("dish: \(dish)")
-//
-//    if !ingredients.contains(ingredient) {
-//        dish.append(dishName)
-//    }
-//    print("dish: \(dish)")
+
+    let valid = map.filter { (i) -> Bool in i.value.count >= 2 }
+        .sorted { $0.key.compare($1.key) == .orderedAscending }
+        .map{[$0.key] + $0.value.sorted{ $0.compare($1) == .orderedAscending }}
+
+
+    return valid
 }
+
+print(groupingDishes(dishes: dishes))
+
+
+
+
+
+
