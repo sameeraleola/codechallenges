be3952f9b1e2c4b85d48d0e3196841d5bf2e211e
Sameera Leola <sameeraleola@gmail.com>
2020-09-30 02:40:17 -0500

    Completed remove duplicates from linked list

--- removeDuplicatesfromUnorderedList.playground/Contents.swift
+++ removeDuplicatesfromUnorderedList.playground/Contents.swift
@@ -1,7 +1,9 @@
 import UIKit
 
 /*
- Remove duplicates from an unsorted linked list
+ Remove duplicates from sorted linked list
+ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ 
  https://www.geeksforgeeks.org/remove-duplicates-from-an-unsorted-linked-list/
  https://stackoverflow.com/questions/45136115/swift-linked-list-remove-duplicates
  */
