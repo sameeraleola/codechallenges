882ef2567ea09748a44987289fb3655770961e6f
Sameera Leola <sameeraleola@gmail.com>
2020-10-14 21:00:20 -0500

    Are following patterns

--- arefollowingpatterns.playground/Contents.swift
+++ arefollowingpatterns.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
+// Code Signal : Are Following Patterns
 
-var str = "Hello, playground"
+func are_following_patterns(strings: [String], patterns: [String]) -> Bool {
+    // If the string and patterns are not the same size than we're done.
+    if strings.count != patterns.count {
+        return false
+    }
+    
+    // If there's only one value it's following
+    if strings.count == 1 {
+        return true
+    }
+    
+    
+    
+    return false
+}
+
+
+
