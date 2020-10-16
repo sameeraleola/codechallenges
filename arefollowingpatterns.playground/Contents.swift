bcd5354e28800668b820b9af4f1cf5eb67baa119
Sameera Leola <sameeraleola@gmail.com>
2020-10-15 22:58:49 -0500

    Still working on arefollowingpatterns

--- arefollowingpatterns.playground/Contents.swift
+++ arefollowingpatterns.playground/Contents.swift
@@ -7,15 +7,22 @@ func are_following_patterns(strings: [String], patterns: [String]) -> Bool {
         return false
     }
     
-    // If there's only one value it's following
-    if strings.count == 1 {
-        return true
-    }
+    // Dictionary to parse the string and pattern
+    var strDict : [String : Int] = [:]
+    var patDict : [String : Int] = [:]
     
+    for index in 0 ..< strings.count {
+        print(strings[index])
+//    if strDict.indexForKey("someKey") != nil {
+//        // the key exists in the dictionary
+//    }
+    }
     
     
     return false
 }
+
+print(are_following_patterns(strings: ["aaa", "bbb", "ccc"], patterns: ["cat", "dog", "bird"]))
 
 
 
