8afbda1edccc49aef3c4297b47362bd7967f27c9
Sameera Leola <sameeraleola@gmail.com>
2020-10-23 23:00:39 -0500

    Completed condense linked list challenge in Swift

--- condenselinkedlist.playground/Contents.swift
+++ condenselinkedlist.playground/Contents.swift
@@ -1,47 +1,60 @@
-import Foundation
+import UIKit
 
-class Node<Value> {     // We want to be able to store any data type within our linked list so we will specify a generic data type.
-    var value: Value
+
+//  https://www.youtube.com/watch?v=e2TjJMhe08U&ab_channel=LucidProgramming
+
+// Create node class
+class Node<Int> {
+    var value: Int
     var next: Node?
     
-    init(value :Value, next: Node? = nil) {
+    init(value: Int, next: Node? = nil) {
         self.value = value
         self.next = next
     }
 }
 
-extension Node: CustomStringConvertible {
-    var description: String {
-        guard let next = next else {
-            return "\(value)"
-        }
-        
-        return "\(value) -> " + String(describing: next) + " "
-    }
-}
-
-// 3. Create nodes.
+// Create the nodes
 let node1 = Node(value: 1)
-let node1_2 = Node(value: 1)
 let node2 = Node(value: 2)
-let node2_2 = Node(value: 2)
 let node3 = Node(value: 3)
 let node4 = Node(value: 4)
-let node4_2 = Node(value: 4)
 let node5 = Node(value: 5)
+let node5_2 = Node(value: 5)
 let node6 = Node(value: 6)
 let node7 = Node(value: 7)
-
-node1.next  = node6
-node6.next = node1_2
-node1_2.next = node4
-node4.next = node2
-node2.next = node4_2
-node4.next = nil
+let node7_2 = Node(value: 7)
 
 
-print(head)
+// Create the linked list
+node5.next = node2
+node2.next = node1
+node1.next = node5_2
+node5_2.next = node4
+node4.next = node7
+node7.next = node3
+node3.next = node7_2
+node7_2.next = nil
 
+func condense_linked_list(node: Node<Int>?) -> Node<Int> {
 
+    var curr = node
+    var prev: Node<Int>?
+    
+    var valuesDict: [Int: Int] = [:]
+    
+    while let nextNode = curr {
+        if (valuesDict[nextNode.value] != nil) {
+            prev?.next = nextNode.next
+        } else {
+            valuesDict[nextNode.value] = 1
+            prev = nextNode
+        }
+        curr = prev?.next
+    }
+    
+    print("The valuesDict contains \(valuesDict)")
+    return node!
+}
 
-
+condense_linked_list(node: node5)
--- endorsement/condense_linked_list.playground/Contents.swift
+++ endorsement/condense_linked_list.playground/Contents.swift
@@ -1,9 +1,57 @@
 import UIKit
 
-// Create linked list
-
-// End linked list
-
-func condense_linked_list(node: Node?) {
+// Create node class
+class Node<Int> {
+    var value: Int
+    var next: Node?
     
+    init(value: Int, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
 }
+
+// Create the nodes
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+let node4 = Node(value: 4)
+let node5 = Node(value: 5)
+let node5_2 = Node(value: 5)
+let node6 = Node(value: 6)
+let node7 = Node(value: 7)
+let node7_2 = Node(value: 7)
+
+
+// Create the linked list
+node5.next = node2
+node2.next = node1
+node1.next = node5_2
+node5_2.next = node4
+node4.next = node7
+node7.next = node3
+node3.next = node7_2
+node7_2.next = nil
+
+func condense_linked_list(node: Node<Int>?) -> Node<Int> {
+
+    var curr = node
+    var prev: Node<Int>?
+    
+    var valuesDict: [Int: Int] = [:]
+    
+    while let nextNode = curr {
+        if (valuesDict[nextNode.value] != nil) {
+            prev?.next = nextNode.next
+        } else {
+            valuesDict[nextNode.value] = 1
+            prev = nextNode
+        }
+        curr = prev?.next
+    }
+    
+    print("The valuesDict contains \(valuesDict)")
+    return node!
+}
+
+condense_linked_list(node: node5)
